f5ff30e535a1366a4586951438b765cb4802efb7
Jeffrey Golditch <jeffreygolditch@Jeffreys-MacBook-Pro.local>
2019-12-24 14:09:42 -0500

    Added messagebox

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Chatkit from '@pusher/chatkit-client'
 import MessageList from './MessageList'
+import SendMessageForm from './SendMessageForm'
 
 class ChatScreen extends Component {
   constructor(props) {
@@ -10,6 +11,14 @@ class ChatScreen extends Component {
       currentRoom: {},
       messages: []
     }
+    this.sendMessage = this.sendMessage.bind(this)
+  }
+
+  sendMessage(text) {
+    this.state.currentUser.sendMessage({
+      text,
+      roomId: this.state.currentRoom.id
+    })
   }
 
   componentDidMount() {
@@ -72,6 +81,7 @@ class ChatScreen extends Component {
             <MessageList
               messages={this.state.messages}
               style={styles.chatList}/>
+            <SendMessageForm onSubmit={this.sendMessage}/>
           </section>
         </div>
       </div>
