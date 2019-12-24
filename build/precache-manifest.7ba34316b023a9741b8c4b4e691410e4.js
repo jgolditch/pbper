3be1adac6eb84e16b01593b4d282ef3ba2e85583
Jeffrey Golditch <jeffreygolditch@Jeffreys-MacBook-Pro.local>
2019-12-24 15:14:47 -0500

    Hosted on github pages

---
+++ build/precache-manifest.7ba34316b023a9741b8c4b4e691410e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a7c503c56623c41d187dc1b601cc82",
+    "url": "/pbper/index.html"
+  },
+  {
+    "revision": "e2ffdefc3699943b50b7",
+    "url": "/pbper/static/css/main.872a695f.chunk.css"
+  },
+  {
+    "revision": "77f347d978ef008ca0bc",
+    "url": "/pbper/static/js/2.734d62e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pbper/static/js/2.734d62e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2ffdefc3699943b50b7",
+    "url": "/pbper/static/js/main.74273052.chunk.js"
+  },
+  {
+    "revision": "10b76b5e15beb3268534",
+    "url": "/pbper/static/js/runtime-main.690d5e3e.js"
+  }
+]);
