b8b39c921a82b1dbe24d56b70072cb08faf6202a
Jeffrey Golditch <jeffreygolditch@Jeffreys-MacBook-Pro.local>
2019-12-26 22:24:00 -0500

    Updates

---
+++ precache-manifest.10ee993a1fde26f020f6d8c53e23645e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64807394dabc9df5c6216de020e81bf",
+    "url": "/pbper/index.html"
+  },
+  {
+    "revision": "939dcb36413561c55162",
+    "url": "/pbper/static/css/main.872a695f.chunk.css"
+  },
+  {
+    "revision": "77f347d978ef008ca0bc",
+    "url": "/pbper/static/js/2.734d62e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pbper/static/js/2.734d62e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "939dcb36413561c55162",
+    "url": "/pbper/static/js/main.d9177a24.chunk.js"
+  },
+  {
+    "revision": "10b76b5e15beb3268534",
+    "url": "/pbper/static/js/runtime-main.690d5e3e.js"
+  }
+]);
